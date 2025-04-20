ce0c31eece66b0d1ffe5b69a404e2ebc039e7898
anilohilmy <hilmy@anilo.id>
2025-04-20 13:25:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const { route } = require('./router');
+const { route } = require('./routers');
 
 app.use(express.json());
 app.use(route);
